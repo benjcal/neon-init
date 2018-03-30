643ba44b68bae434064033fdcbb0f0c2c76cd4e1
Benjamin Calderon <benj.calderon@gmail.com>
2018-03-30 16:15:57 -0400

    changed copy for curl and removed name change

--- neon-init.sh
+++ neon-init.sh
@@ -10,20 +10,20 @@ fi
 mkdir -p native/src
 
 # download template files
-# needs to be changed for curl -o later...
-cp templates/Cargo.toml native/
-cp templates/build.rs native/
-cp templates/lib.rs native/src/
+curl -o native/Cargo.toml https://raw.githubusercontent.com/benjcal/neon-init/master/templates/Cargo.toml
+curl -o native/build.rs https://raw.githubusercontent.com/benjcal/neon-init/master/templates/build.rs
+curl -o native/src/lib.rs https://raw.githubusercontent.com/benjcal/neon-init/master/templates/src/lib.rs
 
-NAME=$(git show --format="%an <%ae>" |head -n 1)
+# NAME=$(git show --format="%an <%ae>" |head -n 1)
 
-if [ -z $(echo $NAME | grep "fatal") ]
-then
-    echo "error with git"
-    NAME=$USER
-fi
+# if [ -z $(echo $NAME | grep "fatal") ]
+# then
+#     echo "error with git"
+#     NAME=$USER
+# fi
 
-sed -i ".toml" "s/\"name\"/\"$NAME\"/g" native/Cargo
+# # change author name for author name
+# sed -i ".toml" "s/\"name\"/\"$NAME\"/g" native/Cargo
 
 npm install neon-cli
 
@@ -34,4 +34,3 @@ let pp = JSON.parse(p); \
 Object.assign(pp.scripts, {'neon-build': 'neon build'}); \
 fs.writeFileSync('package.json~', p); \
 fs.writeFileSync('package.json', JSON.stringify(pp, null, 2));"
-

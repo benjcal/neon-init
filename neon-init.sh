3e6baed26064a1714d81670a5590df882acb9eaa
Benjamin Calderon <benj.calderon@gmail.com>
2018-03-30 16:42:16 -0400

    clean up and removed user questions

--- neon-init.sh
+++ neon-init.sh
@@ -1,49 +1,32 @@
 #!/bin/sh
 
 # make "native" folder
-echo -n "Do you want to enable create 'native' folder? (y/n)? "
-read answer
-if echo "$answer" | grep -iq "^y" ;
+if [ -d native ]
 then
-    if [ -d native ]
-    then
-        echo "'native' folder exist"
-        exit 1
-    fi
-
-    mkdir -p native/src
-
-    # download template files
-    curl -o native/Cargo.toml https://raw.githubusercontent.com/benjcal/neon-init/master/templates/Cargo.toml
-    curl -o native/build.rs https://raw.githubusercontent.com/benjcal/neon-init/master/templates/build.rs
-    curl -o native/src/lib.rs https://raw.githubusercontent.com/benjcal/neon-init/master/templates/src/lib.rs
-else
-    echo "can't install neon without a native folder"
+    echo "Error: 'native' folder exist"
     exit 1
 fi
 
-echo -n "Do you want to enable 'npm run build' for this project? (y/n)? "
-read answer
-if echo "$answer" | grep -iq "^y" ;
+mkdir -p native/src
+
+# download template files
+curl -o native/Cargo.toml https://raw.githubusercontent.com/benjcal/neon-init/master/templates/Cargo.toml
+curl -o native/build.rs https://raw.githubusercontent.com/benjcal/neon-init/master/templates/build.rs
+curl -o native/src/lib.rs https://raw.githubusercontent.com/benjcal/neon-init/master/templates/src/lib.rs
+
+if [ -f package.json ]
 then
-
-    if [ -f package.json ]
-    then
-        echo "package.json not found"
-        echo "generate one using 'npm init'"
-        exit 1
-    fi
-
-    npm install neon-cli
-
-    # add neon-build to package.json
-    node -e "const fs = require('fs'); \
-    let p = fs.readFileSync('package.json', 'utf8'); \
-    let pp = JSON.parse(p); \
-    Object.assign(pp.scripts, {'neon-build': 'neon build'}); \
-    fs.writeFileSync('package.json~', p); \
-    fs.writeFileSync('package.json', JSON.stringify(pp, null, 2));"
-else
-    echo "you can build the neon library by running"
-    echo "./node_modules/neon-cli/bin/cli.js build"
+    echo "package.json not found"
+    echo "generate one using 'npm init'"
+    exit 1
 fi
+
+npm install neon-cli
+
+# add neon-build to package.json
+node -e "const fs = require('fs'); \
+let p = fs.readFileSync('package.json', 'utf8'); \
+let pp = JSON.parse(p); \
+Object.assign(pp.scripts, {'neon-build': 'neon build'}); \
+fs.writeFileSync('package.json~', p); \
+fs.writeFileSync('package.json', JSON.stringify(pp, null, 2));"
